a56ac62e6dc1defd8b8d4c42dcab395394ada589
Sergey Milyukov <serrguk@gmail.com>
2022-11-09 10:58:24 +0700

    Project modified

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,11 @@ public class Main {
         System.out.println("Гравитационная постоянная примерно равна " + SpaceSpeed.G + " м^3/кг*с^2");
         System.out.println("Средний радиус Земли равен " + SpaceObject.EARTH.getRadius() + " метров");
         System.out.println("Масса Земли равна " + SpaceObject.EARTH.getMass() + " килограммов\n");
-        String sampleForV1 = "Первая космическая скорость для объекта %s равна %f метров в секунду\n";
-        String sampleForV2 = "Вторая космическая скорость для объекта %s равна %f метров в секунду\n";
 
-        System.out.printf(sampleForV1, SpaceObject.EARTH, SpaceSpeed.getOrbitalSpeed(SpaceObject.EARTH.getMass(), SpaceObject.EARTH.getRadius()));
-        System.out.printf(sampleForV2, SpaceObject.EARTH, SpaceSpeed.getEscapeSpeed(SpaceObject.EARTH.getMass(), SpaceObject.EARTH.getRadius()));
-        System.out.printf(sampleForV1, SpaceObject.SUN, SpaceSpeed.getOrbitalSpeed(SpaceObject.SUN.getMass(), SpaceObject.SUN.getRadius()));
-        System.out.printf(sampleForV2, SpaceObject.SUN, SpaceSpeed.getEscapeSpeed(SpaceObject.SUN.getMass(), SpaceObject.SUN.getRadius()));
+        System.out.println(SpaceSpeed.isEscape(SpaceObject.EARTH.getMass(), SpaceObject.EARTH.getRadius()));
 
-        System.out.println(SpaceSpeed.getEscapeSpeed(BigDecimal.valueOf(30.0E100), BigDecimal.valueOf(50000)));
-        System.out.println(SpaceSpeed.getOrbitalSpeed(BigDecimal.valueOf(30.0E100), BigDecimal.valueOf(50000)));
+        System.out.println(SpaceSpeed.isEscape(BigDecimal.valueOf(50.0154E100), BigDecimal.valueOf(50000.0)));
+        System.out.println(SpaceSpeed.isOrbital(SpaceObject.SUN.getMass(), SpaceObject.SUN.getRadius()));
+        System.out.println(SpaceSpeed.isEscape(SpaceObject.SUN.getMass(), SpaceObject.SUN.getRadius()));
     }
 }
--- src/SpaceSpeed.java
+++ src/SpaceSpeed.java
@@ -7,22 +7,37 @@ public class SpaceSpeed {
     public static final BigDecimal G = BigDecimal.valueOf(6.6743E-11);// m^3 / kg * s^2 - гравитационная постоянная
     public static final BigDecimal SPEED_OF_LIGHT = BigDecimal.valueOf(299_792_458);// m/c - скорость света
 
-    public static BigDecimal getEscapeSpeed(@NotNull BigDecimal mass, BigDecimal radius) {
-        BigDecimal escapeSpeed = (BigDecimal.valueOf(2).sqrt(MathContext.DECIMAL32)).multiply((G.multiply(mass.divide(radius, MathContext.DECIMAL32))).sqrt(MathContext.DECIMAL32));
-        if (escapeSpeed.compareTo(SPEED_OF_LIGHT) > 0) {
-            System.out.println("Невозможно покинуть объект, вторая космическая скорость превышает скорость света.");
+    private static BigDecimal getEscapeSpeed(@NotNull BigDecimal mass, BigDecimal radius) {
+        BigDecimal escapeSpeed = (BigDecimal.valueOf(2)
+                .sqrt(MathContext.DECIMAL32))
+                .multiply((G.multiply(mass.divide(radius, MathContext.DECIMAL32)))
+                        .sqrt(MathContext.DECIMAL32));
+        if (escapeSpeed.compareTo(SPEED_OF_LIGHT) >= 0) {
             return BigDecimal.ZERO;
         }
         return escapeSpeed;
     }
 
-    public static BigDecimal getOrbitalSpeed(@NotNull BigDecimal mass, BigDecimal radius) {
+    private static BigDecimal getOrbitalSpeed(@NotNull BigDecimal mass, BigDecimal radius) {
         BigDecimal orbitalSpeed = (G.multiply(mass.divide(radius, MathContext.DECIMAL32))).sqrt(MathContext.DECIMAL32);
-        if (orbitalSpeed.compareTo(SPEED_OF_LIGHT) > 0) {
-            System.out.println("Невозможно выйти на орбиту объекта, орбитальная скорость превышает скорость света.");
+        if (orbitalSpeed.compareTo(SPEED_OF_LIGHT) >= 0) {
             return BigDecimal.ZERO;
         }
         return orbitalSpeed;
+    }
+
+    public static String isEscape(BigDecimal mass, BigDecimal radius) {
+        BigDecimal result = getEscapeSpeed(mass, radius);
+        return result.equals(BigDecimal.ZERO) ?
+                "Невозможно покинуть объект, вторая космическая скорость больше или равна скорости света" :
+                "Вторая космическая скорость равна " + result + " метров в секунду";
+    }
+
+    public static String isOrbital(BigDecimal mass, BigDecimal radius) {
+        BigDecimal result = getOrbitalSpeed(mass, radius);
+        return result.equals(BigDecimal.ZERO) ?
+                "Невозможно выйти на орбиту, первая космическая скорость больше или равна скорости света" :
+                "Первая космическая скорость равна " + result + " метров в секунду";
     }
 
 }
